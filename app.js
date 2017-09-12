d18352ea960af0c1d075c96180f5b1abb1b800c8
achmadridho <Souleater999>
2017-09-12 14:46:12 +0700

    absensi

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var database = require('./setup/database');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const database = require('./setup/database');
+const configs = require('./setup/configs.json');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const debug = require('debug')('Manbaul-Huda:website');
+const http = require('http');
+const flash = require('express-flash')
 
-var debug = require('debug')('manbaul-huda-absensi:website');
-var http = require('http');
+const app = express();
 
-var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -20,31 +24,38 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(flash())
 app.use(express.static(path.join(__dirname, 'public')));
-
-
+app.use(session({
+    secret: "absensiSMP48",
+    resave: false,
+    saveUninitialized: true,
+    store: new MongoStore({ url: configs.mongodb_uri }),
+    cookie: { secure: !true }
+}));
 
 
 database.connect(function (err, db) {
     if(err){
         console.log(err);
     } else {
+        app.use(function (req, res, next) {
+            res.locals.messages = require('express-messages')(req, res);
+            next();
+        });
         app.use(function(err, req, res, next) {
             res.locals.message = err.message;
             res.locals.error = req.app.get('env') === 'development' ? err : {};
             res.status(err.status || 500);
             res.render('error');
         });
-
-        //---- test
-
-
-        var port = normalizePort(process.env.PORT || '3051');
+        var port = normalizePort(process.env.PORT || '8012');
         app.set('port', port);
         var server = http.createServer(app);
         server.listen(port);
         server.on('error', onError);
         server.on('listening', onListening);
+        var io = require('socket.io').listen(server);
         function normalizePort(val) {
             var port = parseInt(val, 10);
             if (isNaN(port)) {
@@ -87,12 +98,25 @@ database.connect(function (err, db) {
             debug('Listening on ' + bind);
         }
 
+        io.on('connection', function(socket){
+            console.log('a user connected');
+            socket.on('disconnect', function(){
+                console.log('user disconnected');
+            });
+        });
+        app.io = io;
         app.db = db;
         module.exports = app;
+        const rmqConnection = require('./rmq/connection');
+        rmqConnection.connect();
         var index = require('./routes/index');
+        var users = require('./routes/users');
+        var absensi = require('./routes/absensi');
 
 
         app.use('/', index);
+        app.use('/users', users);
+        app.use('/absensi', absensi);
 
 
 
